ca7f0bb978e2cdc05964c5a0efcdca37f87acff6
Stanley Sim <simyseng@gmail.com>
2025-12-26 09:37:05 +0800

    Initial Commit

---
+++ Tasks_2026/Tasks_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  Tasks_2026App.swift
+//  Tasks_2026
+//
+//  Created by Stanley Sim on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Tasks_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
